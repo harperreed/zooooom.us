3fdd34a8af7c6ab42aaa902feb1b3448d8f750d1
Harper Reed <harper@nata2.org>
2020-04-05 22:16:42 -0500

    updated generator and config.json

--- generate_config.py
+++ generate_config.py
@@ -1,17 +1,23 @@
 import os
 import json
+
+base_config_file = 'screensaver_config.json'
 config_file = 'static/config.json'
 media_folder = 'static/media/'
 domain = "suspicious-kirch-4c179d.netlify.com"
 
 subfolders = [ f.path for f in os.scandir(media_folder) if f.is_dir() ]
-config = {}
+
+with open(base_config_file) as json_file:
+    config = json.load(json_file)
+    print(config)
 
 videos = []
 for i in subfolders:
     dirhash = i.replace(media_folder,"")
     url = "https://" + domain + "/media/" + dirhash + "/prog_index.m3u8"
     videos.append(url)
+
 config['videos'] = videos
 
 with open(config_file, 'w') as outfile:
