f4fb632051bedb70b0b8ee8084c0740e1c479876
Harper Reed <harper@nata2.org>
2020-04-08 20:25:54 -0500

    added some more videos

--- render_mp4.sh
+++ render_mp4.sh
@@ -7,9 +7,15 @@ for i in ${source_dir}/*;
   do name=`basename "$i" `
   name=`echo "$name" | cut -d'.' -f1`
   name=`echo "$name" | md5`
+  destination_file="${render_dir}/${name}.mp4"
+  destination_folder="${render_dir}/${name}"
 
-  ffmpeg -i "$i" -vcodec libx264 -crf 30  -an "${render_dir}/${name}.mp4"
-  mkdir "${render_dir}/${name}"
-  mediafilesegmenter "${render_dir}/${name}.mp4" -f "${render_dir}/${name}" -t 5
+  if [ ! -f "$destination_file" ]; then
+    ffmpeg -i "$i" -vcodec libx264 -crf 30  -an "${destination_file}"
+    if [ ! -d "$destination_folder" ]; then
+      mkdir "${destination_folder}"
+      mediafilesegmenter "${destination_file}" -f "${destination_folder}" -t 5
+    fi
+  fi
 
 done
